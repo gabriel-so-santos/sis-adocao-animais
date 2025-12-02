c9d19759ccb8bcaedba7c9c1317bf0a3938444e6
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2025-12-02 18:04:06 -0300

    Create repositories

--- repositories/adopter_repo.py
+++ repositories/adopter_repo.py
@@ -1,5 +1,82 @@
-from base_repo import BaseRepository
+from models.adopter_model import AdopterModel
+from domain.people.adopter import Adopter, HousingType
+        
 
-class AdopterRepository(BaseRepository):
-    """Interface específica para repositório de animais."""
-    pass
+class AdopterRepository:
+   
+    def __init__(self, session):
+        self.session = session
+    
+    def to_domain(self, adopter_model: AdopterModel) -> Adopter:
+        """Converte o Model (SQL) em uma entidade de domínio"""
+        
+        return Adopter(
+            id = adopter_model.id,
+            name = adopter_model.name,
+            age = adopter_model.age,
+            housing_type = HousingType[adopter_model.housing_type],    
+            usable_area = adopter_model.usable_area,
+            has_pet_experience = adopter_model.has_pet_experience,
+            has_children_at_home = adopter_model.has_children_at_home,
+            has_other_animals = adopter_model.has_other_animals
+        )
+
+    # ---- Create ----
+    def save(self, adopter) -> AdopterModel:
+        """Salva uma entidade Adopter no banco"""
+
+        adopter_db = AdopterModel(
+            id = adopter.id,
+            name = adopter.name,
+            age = adopter.age,
+            housing_type = adopter.housing_type.name,           # Enum -> str
+            usable_area = adopter.usable_area,
+            has_pet_experience = adopter.has_pet_experience,
+            has_children_at_home = adopter.has_children_at_home,
+            has_other_animals = adopter.has_other_animals
+        )
+
+        self.session.add(adopter_db)
+        self.session.commit()
+        self.session.refresh(adopter_db)
+        return adopter_db
+
+    # ---- Read ----
+    def list_all(self) -> list[AdopterModel]:
+        """Retorna uma lista de todos os adotates cadastrados no banco"""
+        return self.session.query(AdopterModel).all()
+
+    def get_by_id(self, id: int) -> AdopterModel:
+        """Retorna um adotante cadastrado no banco"""
+        return self.session.get(AdopterModel, id)
+
+    # ---- Update ----
+    def update(self, adopter) -> AdopterModel|None:
+        """Atualiza um registro existente no banco a partir de um objeto de domínio"""
+        adopter_db = self.session.get(AdopterModel, adopter.id)
+
+        if not adopter_db:
+            return None
+
+        adopter_db.name = adopter.name
+        adopter_db.age = adopter.age
+        adopter_db.housing_type = adopter.housing_type.name        # Enum -> str
+        adopter_db.usable_area = adopter.usable_area
+        adopter_db.has_pet_experience = adopter.has_pet_experience
+        adopter_db.has_children_at_home = adopter.has_children_at_home
+        adopter_db.has_other_animals = adopter.has_other_animals
+
+        self.session.commit()
+        self.session.refresh(adopter_db)
+        return adopter_db
+
+    # ---- Delete ----
+    def delete_by_id(self, id: int) -> bool:
+        adopter_db = self.session.get(AdopterModel, id)
+
+        if not adopter_db:
+            return False
+
+        self.session.delete(adopter_db)
+        self.session.commit()
+        return True
--- repositories/animal_repo.py
+++ repositories/animal_repo.py
@@ -1,5 +1,85 @@
-from base_repo import BaseRepository
+from models.animal_model import AnimalModel
+import json
 
-class AnimalRepository(BaseRepository):
-    """Interface específica para repositório de animais."""
-    pass
+class AnimalRepository:
+    def __init__(self, session):
+        self.session = session
+
+    def to_domain(self, animal_model: AnimalModel):
+        """Converte o Model (SQL) em uma entidade de domínio"""
+        from domain.animals.animal import Animal, Gender, Size
+        from domain.animals.animal_status import AnimalStatus
+        
+        return Animal(
+            id=animal_model.id,
+            species=animal_model.species,
+            breed=animal_model.breed,
+            name=animal_model.name,
+            gender=Gender[animal_model.gender], 
+            age_months=animal_model.age_months,
+            size=Size[animal_model.size],       
+            temperament=json.loads(animal_model.temperament),
+            status=AnimalStatus[animal_model.status] 
+        )
+
+    # ---- Create ----
+    def save(self, animal) -> AnimalModel:
+        """Salva uma entidade Animal no banco"""
+
+        animal_db = AnimalModel(
+            id=animal.id, 
+            species=animal.species,
+            breed=animal.breed,
+            name=animal.name,
+            gender=animal.gender.name,    # Enum -> str
+            age_months=animal.age_months,
+            size=animal.size.name,        # Enum -> str
+            temperament=json.dumps(animal.temperament),
+            status=animal.status.name,    # Enum -> str
+        )
+
+        self.session.add(animal_db)
+        self.session.commit()
+        self.session.refresh(animal_db)
+        return animal_db
+
+    # ---- Read ----
+    def list_all(self) -> list[AnimalModel]:
+        """Retorna uma lista de todos os anmais cadastrados no banco"""
+        return self.session.query(AnimalModel).all()
+
+    def get_by_id(self, id: int) -> AnimalModel:
+        """Retorna um animal cadastrado no banco"""
+        return self.session.get(AnimalModel, id)
+
+    # ---- Update ----
+    def update(self, animal) -> AnimalModel|None:
+        """Atualiza um registro existente no banco a partir de um objeto de domínio"""
+        animal_db = self.session.get(AnimalModel, animal.id)
+
+        if not animal_db:
+            return None
+
+        animal_db.species = animal.species
+        animal_db.breed = animal.breed
+        animal_db.name = animal.name
+        animal_db.gender = animal.gender.name
+        animal_db.age_months = animal.age_months
+        animal_db.size = animal.size.name
+        animal_db.temperament = json.dumps(animal.temperament)
+        animal_db.status = animal.status.name
+
+        self.session.commit()
+        self.session.refresh(animal_db)
+        return animal_db
+
+    # ---- Delete ----
+    def delete_by_id(self, id: int) -> bool:
+        animal_db = self.session.get(AnimalModel, id)
+
+        if not animal_db:
+            return False
+
+        self.session.delete(animal_db)
+        self.session.commit()
+        return True
