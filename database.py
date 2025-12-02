095825a86b10c01b7ea2d030656010a654159ab0
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2025-12-02 18:06:18 -0300

    Create database

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.base_repo import Base
+
+engine = create_engine("sqlite:///data/database.db", echo=False)
+
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    """Cria todas as tabelas definidas pelos modelos ORM."""
+    from models.animal_model import AnimalModel  
+    
+    Base.metadata.create_all(bind=engine)
